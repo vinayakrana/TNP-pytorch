f210632c7bc5bb1eebb0c509b5981df6f002e205
vinayakrana <vinayakrana4444@gmail.com>
2025-05-06 21:11:05 +0530

    made changes to task creation and added convcnp support

--- regression/models/convcnp.py
+++ regression/models/convcnp.py
@@ -3,7 +3,7 @@ import neuralprocesses.torch as nps
 from attrdict import AttrDict
 
 class CONVCNP(torch.nn.Module):
-    def __init__(self, dim_x=1, dim_y=1, likelihood="lowrank"):
+    def __init__(self, dim_x=2, dim_y=1, likelihood="het"):
 
         super(CONVCNP, self).__init__()
         self.likelihood = likelihood
@@ -27,18 +27,32 @@ class CONVCNP(torch.nn.Module):
         outs.loss = -outs.tar_ll
         
         return outs
-    def predict(self, xc, yc, xt, num_samples=1):
+    # def predict(self, xc, yc, xt, num_samples=1):
 
+    #     self.eval()
+    #     with torch.no_grad():
+    #         xc = xc.permute(0, 2, 1)
+    #         xt = xt.permute(0, 2, 1)
+    #         yc = yc.permute(0, 2, 1)
+
+    #         mean, var, _, _ = nps.predict(self.model, xc, yc, xt)
+
+    #         return AttrDict(
+    #             loc=mean.permute(0, 2, 1),
+    #             scale=var.sqrt().permute(0, 2, 1)
+    #         )
+
+    def predict(self, xc, yc, xt, num_samples=1):
         self.eval()
         with torch.no_grad():
             xc = xc.permute(0, 2, 1)
             xt = xt.permute(0, 2, 1)
             yc = yc.permute(0, 2, 1)
 
-            mean, var, _, _ = nps.predict(self.model, xc, yc, xt)
+            # Wrap context as a list of tuples
+            mean, var, _, _ = nps.predict(self.model, [(xc, yc)], xt, num_samples=num_samples)
 
             return AttrDict(
-                loc=mean.permute(0, 2, 1),
+                mean=mean.permute(0, 2, 1),
                 scale=var.sqrt().permute(0, 2, 1)
             )
-
